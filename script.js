0ff28f0a4b8d9d05a196b06059d5cf0a38a93f75
J0hn-4ndr3w <118709598+J0hn-4ndr3w@users.noreply.github.com>
2023-01-21 00:02:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popular.desc&api_key=04c35731a5ee918f014970082a0088b1&page=2";
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popular.desc&api_key=04c35731a5ee918f014970082a0088b1&page=20";
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
